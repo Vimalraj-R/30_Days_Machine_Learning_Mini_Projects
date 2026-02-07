14efbba1a9cace3da8492f8e07ff0d1337323b66
Vimalraj Ravi <161675933+Vimalraj-R@users.noreply.github.com>
2026-02-07 14:07:52 +0530

    Refactor streamlit.py for improved path handling

--- Day1_House_Prediction/streamlit.py
+++ Day1_House_Prediction/streamlit.py
@@ -4,14 +4,15 @@ import numpy as np
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
 import os
-import pandas as pd
 
-BASE_DIR = os.path.dirname(C:/Users/Vimalraj/OneDrive/Desktop/30_days_Machine_learning_challenge/Day1_House_Prediction/)
-df = os.path.join(BASE_DIR, "Housing.csv")
+# Get current directory
+BASE_DIR = os.path.dirname(__file__)
+data_path = os.path.join(BASE_DIR, "Housing.csv")
+
+# Load dataset
+df = pd.read_csv(data_path)
 
 st.title("🏠 House Price Prediction App Using Streamlit")
-
-df = pd.read_csv(data_path)
 
 # Convert yes/no to 1/0
 df.replace({'yes': 1, 'no': 0}, inplace=True)
@@ -38,13 +39,8 @@ bathrooms = st.number_input("Bathrooms", min_value=1, max_value=10, step=1)
 stories = st.number_input("Stories", min_value=1, max_value=5, step=1)
 parking = st.number_input("Parking Spaces", min_value=0, max_value=5, step=1)
 
-# Prediction button
+# Prediction
 if st.button("Predict Price"):
     input_data = np.array([[area, bedrooms, bathrooms, stories, parking]])
     prediction = model.predict(input_data)
     st.success(f"💰 Predicted House Price: ₹ {int(prediction[0]):,}")
-
-
-
-
-
